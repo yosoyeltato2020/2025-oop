a54bb03fe6390a964984c32dabe03348e05dc8e4
yosoyeltato2020 <yosoyeltato2020@hotmail.com>
2025-05-15 13:22:20 +0200

    lo grabo para continuar despues

---
+++ taberna.py
@@ -0,0 +1,45 @@
+class Copa:
+    def __init__(self, capacidad, cantidad):        
+        self.capacidad = int(capacidad)             
+        self.cantidad = float(cantidad)                
+
+    def llenar(self):
+        self.cantidad = 1.0
+
+    def vaciar(self, cantidad):
+        self.cantidad -= cantidad
+        print(f"En la copa queda {self.cantidad}")
+
+class Parroquiano:
+    def __init__(self, copa, dinero):
+        self.copa = copa
+        self.dinero = dinero
+
+
+        self.pedir()  
+        self.beber()
+        self.beber()
+        self.beber()
+        self.beber()
+        self.beber()
+        self.beber()
+
+
+    def pedir(self):
+        if self.dinero >= 1:
+            self.copa.llenar()
+            self.dinero -= 1
+            print("El parroquiano tiene dinero y ha pedido una copa.")
+        else:
+            print("No tiene suficiente dinero para pedir asi que hasta luego lucas.")
+
+    def beber(self):
+        if self.copa.cantidad > 0:
+            self.copa.vaciar(0.25)
+            print("El parroquiano bebe una parte de la copa.")
+        else:
+            print("La copa está vacía.")
+
+
+la_copa = Copa(capacidad=1000, cantidad=1.0) 
+parroquiano = Parroquiano(copa=la_copa, dinero=5)  
