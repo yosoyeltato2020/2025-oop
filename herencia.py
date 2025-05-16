b31c5f4598ff26e1e647c6224f114b9e30a43bfa
elMestre <carmesce@gmail.com>
2025-05-16 12:38:23 +0200

    Super

--- herencia.py
+++ herencia.py
@@ -1,43 +1,55 @@
 def caosZoologico():
-    mapachio = Animal("Mapachio")
-    print(isinstance(mapachio, Animal))
-    print(isinstance(mapachio, Perro))
-    mapachio.come()
-
-    firulais = Perro("Firulais")
+    firulais = Perro("Firulais", "ChowChow")
     firulais.come()
     firulais.pasea()
 
-    myLittle = Pony("My Little")
-    myLittle.come()
-    myLittle.pasea()
+    mizgo = Larva("Mizgo")
+    mizgo.come()
+
+    gizmo = Gremlin("Gizmo")
+    gizmo.come()
+
+    firulais.registrar()
+    gizmo.registrar()
 
 
 # Definimos una clase padre
 class Animal:
     def __init__(self, nombre):
         self.nombre = nombre
+        self.comidas = 0
+        self.chip = 1234654
 
     def come(self):
-        print(f"El Animal {self.nombre} ha comido")
+        self.comidas += 1
+        print(f"El Animal {self.nombre} ha comido {self.comidas} veces")
+
+    def registrar(self):
+        print(f"Hemos registado a {self.nombre} en la BBDD del gobierno")
 
 
-# la Super Clase de Domestico es Animal
-# Perro es Sub Clase de Domestico
-# Domestico es Super Clase de Perro
-# Domestico es Sub clase de Animal
-class Domestico(Animal):
+class Perro(Animal):
+    def __init__(self, nombre, raza):
+        super().__init__(nombre)
+        self.raza = raza
+
     def pasea(self):
-        print(f"El Animal domestico {self.nombre} se pasea")
+        print(f"El Perro {self.raza} llamado {self.nombre} se pasea")
 
 
-# Creamos una clase hija que hereda de la padre
-class Perro(Domestico):
-    pass
+# Larva sobrescribe el método
+class Larva(Animal):
+    def come(self):
+        self.comidas += 0.1
+        print("Me fabrica seda")
+        print("La Larva SE TRANFORMA en mariposa")
 
 
-class Pony(Domestico):
-    pass
+# Gremiln EXTIENDE el método
+class Gremlin(Animal):
+    def come(self):
+        super().come()                  # Primero ejecuta el comportamiento del super
+        print("Gremlin se TRANSFORMA")  # Luego expande la funcionalidad
 
 
 if __name__ == "__main__":
